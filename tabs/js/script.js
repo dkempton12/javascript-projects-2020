416013823bf6685fbd71a3c2dfa72df2ba231ced
Dan Kempton <kemptondan@gmail.com>
2020-02-11 18:41:41 -0500

    Updating ARIA

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -1,7 +1,7 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role=tab]');
 // creating an array of the tabs to loop through
-const tabPanels = Array.from(tabs.querySelectorAll('[role="tab-panel"]'));
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
   // hide all tab panels
