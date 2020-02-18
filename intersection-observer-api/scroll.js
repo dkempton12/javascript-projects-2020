21fefad111fba7a3e5812b8f67f78163b1bb8f9f
Dan Kempton <kemptondan@gmail.com>
2020-02-18 13:59:16 -0500

    Adding observation callback function

--- intersection-observer-api/scroll.js
+++ intersection-observer-api/scroll.js
@@ -1,3 +1,10 @@
-const terms = document.querySelector('.terms');
-
+const text = document.querySelector('.text');
+console.log(terms);
 const acceptButton = document.querySelector('.accept');
+
+function observationCallBack(payback) {
+  if (payback[0].intersectionRatio === 1) {
+    acceptButton.disabled = false;
+    observationCallBack.unobserve(terms.lastElementChild);
+  }
+}
