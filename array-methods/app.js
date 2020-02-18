9b1d5c6c0853598e0d697651f1dfc211f5a35405
Dan Kempton <kemptondan@gmail.com>
2020-02-18 17:57:30 -0500

    Adding map method

--- array-methods/app.js
+++ array-methods/app.js
@@ -1,1 +1,6 @@
 // Map method
+const countries = ['United States', 'Canada', 'Mexico', 'England', 'Jamaica'];
+
+const countriesLowerCase = countries.map(country => country.toLowerCase());
+
+console.log(countriesLowerCase);
