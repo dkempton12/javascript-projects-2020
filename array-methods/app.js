843b86b106c4ba7271229b7701eeb541fc71cf8b
Dan Kempton <kemptondan@gmail.com>
2020-02-18 18:05:56 -0500

    Adding forEach method

--- array-methods/app.js
+++ array-methods/app.js
@@ -1,3 +1,11 @@
+// forEach method
+const mealOrders = [100, 110, 98, 120, 60];
+const mealTaxMA = 0.0625;
+
+const mealsWithTax = mealOrders.forEach(
+  mealOrder => mealOrder * mealTaxMA
+  );
+
 // Map method
 const countries = ['United States', 'Canada', 'Mexico', 'England', 'Jamaica'];
 
