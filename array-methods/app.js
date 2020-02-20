798c3ae2f54d760af2c0faa7d0ede80a20dc87eb
Dan Kempton <kemptondan@gmail.com>
2020-02-20 15:18:52 -0500

    Adding filter method example

--- array-methods/app.js
+++ array-methods/app.js
@@ -9,3 +9,8 @@ const countries = ['United States', 'Canada', 'Mexico', 'England', 'Jamaica'];
 const countriesLowerCase = countries.map(country => country.toLowerCase());
 
 console.log(countriesLowerCase);
+
+// Filter method
+const words = ['the', 'and', 'from', 'javascript', 'programming'];
+const longerThanThreeLetters = words.filter(word => word.length > 3);
+console.log(longerThanThreeLetters);
