85454941c4bf630f0dd4678374c3df98213a36b9
Dan Kempton <kemptondan@gmail.com>
2020-02-25 18:08:59 -0500

    Adding find method example

--- array-methods/app.js
+++ array-methods/app.js
@@ -14,3 +14,8 @@ console.log(countriesLowerCase);
 const words = ['the', 'and', 'from', 'javascript', 'programming'];
 const longerThanThreeLetters = words.filter(word => word.length > 3);
 console.log(longerThanThreeLetters);
+
+// Find method - finding first number in the array greater than 25 
+const randomNumbers = [6, 8, 12, 22, 26, 50, 88];
+const greaterThan25 = randomNumbers.find(number => number > 25);
+console.log(`First number greater than 25: ${greaterThan25}`);
