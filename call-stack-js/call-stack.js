1579627373e0bcc45b424dd05e23d547fac36ca9
Dan Kempton <kemptondan@gmail.com>
2020-03-03 17:08:12 -0500

    Updating js

--- call-stack-js/call-stack.js
+++ call-stack-js/call-stack.js
@@ -1,7 +1,7 @@
-// In this example, the invocation goes to the top of the stack 
+// In this example, the invocation goes to the top of the stack
 // Next, the multiply function is stacked on top and runs in order to complete
 // the function call
-// Lastly, the call is again on top of the stack and is executed 
+// Lastly, the call is again on top of the stack and is executed
 
 // Function declaration
 const multiply = (a, b) => a * b;
@@ -9,13 +9,12 @@ const multiply = (a, b) => a * b;
 // Invocation
 const result = multiply(4, 4);
 
-
 // Another call stack example
 function firstExample() {
-  return `First example!`;
+	return `First example!`;
 }
 
 function secondExample() {
-  return firstExample() + ` Second example!`;
+	return firstExample() + ` Second example!`;
 }
 secondExample();
