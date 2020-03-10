55191a8dacdf761c3d622e525ac91e2d077901ca
Dan Kempton <kemptondan@gmail.com>
2020-03-10 14:06:46 -0400

    Grabbing the image src info to populate the inner modal with

--- open-close-modal/modal.js
+++ open-close-modal/modal.js
@@ -7,4 +7,8 @@ const outerModal = document.querySelector('.outer-modal');
 function handleButtonClick(event) {
 	const button = event.currentTarget;
 	const card = button.closest('.card');
+	// Grabbing image source
+	const imageSrc = card.querySelector('img').src;
+	const description = card.dataset.description;
+	const title = document.querySelector('h2').textContent;
 }
