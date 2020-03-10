7fc0a9a41cd9889097be77a6ecfa0e150a2cc4a5
Dan Kempton <kemptondan@gmail.com>
2020-03-10 14:01:41 -0400

    Adding button click handler function

--- open-close-modal/modal.js
+++ open-close-modal/modal.js
@@ -2,3 +2,9 @@
 const buttons = document.querySelectorAll('.card button');
 const innerModal = document.querySelector('.inner-modal');
 const outerModal = document.querySelector('.outer-modal');
+
+// Handling button clicks
+function handleButtonClick(event) {
+	const button = event.currentTarget;
+	const card = button.closest('.card');
+}
