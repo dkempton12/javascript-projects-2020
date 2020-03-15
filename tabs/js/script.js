6c07d335825ba47d06e516cace583e04274fd361
Dan Kempton <kemptondan@gmail.com>
2020-03-15 15:53:37 -0400

    Testing

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -4,24 +4,24 @@ const tabButtons = tabs.querySelectorAll('[role=tab]');
 const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
-  // hide all tab panels
-  tabPanels.forEach(panel => {
-    panel.hidden = true;
-  });
-  // mark all button tabs as unselected 
-  tabButtons.forEach(tab => {
-    tab.setAttribute('aria-selected', false);
-  })
-  // Mark the clicked tab as selected
-  event.currentTarget.setAttribute('aria-selected', true);
+	// hide all tab panels
+	tabPanels.forEach((panel) => {
+		panel.hidden = true;
+	});
+	// mark all button tabs as unselected
+	tabButtons.forEach((tab) => {
+		tab.setAttribute('aria-selected', false);
+	});
+	// Mark the clicked tab as selected
+	event.currentTarget.setAttribute('aria-selected', true);
 
-  // Finding the associated tab panel and show it
-  const {id} = event.currentTarget;
+	// Finding the associated tab panel and show it
+	const { id } = event.currentTarget;
 
-  // Finding the panel where the aria-labelledby is equal to 'id'
-  const findTabPanel = tabPanels.find(panel => panel.getAttribute('aria-labeledby') === id);
+	// Finding the panel where the aria-labelledby is equal to 'id'
+	const findTabPanel = tabPanels.find((panel) => panel.getAttribute('aria-labeledby') === id);
 
-  // Setting the clicked tab panel's hidden property to false
-  findTabPanel.hidden = false;
+	// Setting the clicked tab panel's hidden property to false
+	findTabPanel.hidden = false;
 }
-tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
+tabButtons.forEach((button) => button.addEventListener('click', handleTabClick));
