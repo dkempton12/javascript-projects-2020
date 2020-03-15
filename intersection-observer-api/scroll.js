3e70a72bb58bad103015751482064c3e7a4f9a38
Dan Kempton <kemptondan@gmail.com>
2020-03-15 14:04:04 -0400

    Updating observe method

--- intersection-observer-api/scroll.js
+++ intersection-observer-api/scroll.js
@@ -4,15 +4,15 @@ const button = document.querySelector('.accept');
 
 // Displays button if intersectionRatio is equal to 1
 function observationCallBack(payback) {
-  if (payback[0].intersectionRatio === 1) {
-    button.disabled = false;
-    ob.unobserve(text.lastElementChild);
-  }
+	if (payback[0].intersectionRatio === 1) {
+		button.disabled = false;
+		ob.unobserve(text.lastElementChild);
+	}
 }
 // IntersectionObserver API - takes a root (parent) and threshold ratio
 const ob = new IntersectionObserver(observationCallBack, {
-  root: text,
-  threshold: 1,
+	root: text,
+	threshold: 1
 });
 
-ob.observe(text.lastElementChild);
+ob.observe(text.lastElementChild); // Watching for last element in the text div
