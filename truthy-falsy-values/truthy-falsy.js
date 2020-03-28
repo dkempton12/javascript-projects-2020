4ef4be6ee825343f04e5b4b0ace01f3591c02741
Dan Kempton <kemptondan@gmail.com>
2020-03-28 17:46:56 -0400

    Adding forEach method on the values array to check whether values are truthy or falsy

--- truthy-falsy-values/truthy-falsy.js
+++ truthy-falsy-values/truthy-falsy.js
@@ -1,1 +1,14 @@
-const values = [ 0, 1, -10, '', 'string with content', undefined, NaN, null, {}, [] ];
+const values = [ 0, 1, -10, ``, `string with content`, undefined, NaN, null, {}, [] ];
+
+console.group(`Checking if values are Truthy or Falsy`);
+
+values.forEach((value) => {
+	// If value is truthy
+	if (value) {
+		console.table(value, `Value is truthy`);
+		// Else, value is falsy
+	} else {
+		console.table(value, `Value is falsy`);
+	}
+});
+console.groupEnd();
