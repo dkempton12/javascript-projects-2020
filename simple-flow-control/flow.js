23853196ba40b1a84535e24f72bf4d25b0d6ae11
Dan Kempton <kemptondan@gmail.com>
2020-03-28 17:06:35 -0400

    Adding files

---
+++ simple-flow-control/flow.js
@@ -0,0 +1,11 @@
+const age = 50;
+
+if (age >= 70) {
+	console.log(`You age is: ${age}, meaning you're in your 70s!`);
+} else if (age >= 60) {
+	console.log(`You age is: ${age}, meaning you're in your 60s!`);
+} else if (age >= 50) {
+	console.log(`You age is: ${age}, meaning you're in your 50s!`);
+} else {
+	console.log(`You are: ${age} years old!`);
+}
