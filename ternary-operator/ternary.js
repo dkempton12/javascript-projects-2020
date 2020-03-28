7d879a8f11cc7acafd298f36f455e8063a2f58b3
Dan Kempton <kemptondan@gmail.com>
2020-03-28 18:32:13 -0400

    Adding ternary operator example

--- ternary-operator/ternary.js
+++ ternary-operator/ternary.js
@@ -1,11 +1,15 @@
-const count = 0;
-let word;
+// const count = 0;
+// let word;
 
-if (count === 1) {
-	word = `item`;
-} else {
-	word = `items`;
-}
+// if (count === 1) {
+// 	word = `item`;
+// } else {
+// 	word = `items`;
+// }
 
+// const sentence = `You have ${count} ${word} in your cart`;
+// console.log(sentence);
+let count = 0;
+const word = count === 1 ? `item` : `items`;
 const sentence = `You have ${count} ${word} in your cart`;
 console.log(sentence);
