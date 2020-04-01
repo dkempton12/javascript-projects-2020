8bfd00f30798005e2debf4afad30ad7337446206
Dan Kempton <kemptondan@gmail.com>
2020-04-01 17:43:48 -0400

    Adding inventory object

---
+++ find-method-destructuring/find-inventory.js
@@ -0,0 +1,6 @@
+const shellFishInventory = [
+	{ name: 'Clams', quantity: 2000 },
+	{ name: 'Mussels', quantity: 2500 },
+	{ name: 'Oysters', quantity: 3000 },
+	{ name: 'Scallops', quantity: 1000 }
+];
