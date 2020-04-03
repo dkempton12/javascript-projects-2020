f92d7e6133c471c80e240cd4427c943cd16638e2
Dan Kempton <kemptondan@gmail.com>
2020-04-03 18:42:26 -0400

    Refactoring sort method logic

--- sorting-numbers/sort.js
+++ sorting-numbers/sort.js
@@ -1,11 +1,16 @@
 const numbers = [ 100, 1000, 2, 54, 24, 12, 999, 3 ];
 
 const numbersSorted = numbers.sort(function(a, b) {
-	if (a > b) {
-		return 1;
-	} else if (b > a) {
-		return -1;
-	} else {
-		return 0;
-	}
+	// if (a > b) {
+	// 	return 1;
+	// } else if (b > a) {
+	// 	return -1;
+	// } else {
+	// 	return 0;
+	// }
+
+	// Refactored
+	return a - b;
 });
+
+console.log(numbersSorted);
