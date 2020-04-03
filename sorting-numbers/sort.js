1594db14b5cfe7e403f99385ce8f08744011746f
Dan Kempton <kemptondan@gmail.com>
2020-04-03 18:38:45 -0400

    Adding sort method logic

--- sorting-numbers/sort.js
+++ sorting-numbers/sort.js
@@ -1,1 +1,11 @@
 const numbers = [ 100, 1000, 2, 54, 24, 12, 999, 3 ];
+
+const numbersSorted = numbers.sort(function(a, b) {
+	if (a > b) {
+		return 1;
+	} else if (b > a) {
+		return -1;
+	} else {
+		return 0;
+	}
+});
