5992a7bd47fa37c06c97b62acea739e334a907a9
Dan Kempton <kemptondan@gmail.com>
2020-04-08 11:55:11 -0400

    Adding object data

---
+++ flexible-find-function/find.js
@@ -0,0 +1,9 @@
+const celtics = [
+	{ name: 'Kemba Walker', position: 'Point Guard', jerseyNumber: '8' },
+	{ name: 'Jayson Tatum', position: 'Forward', jerseyNumber: '0' },
+	{ name: 'Jaylen Brown', position: 'Guard', jerseyNumber: '8' },
+	{ name: 'Gordon Hayward', position: 'Forward', jerseyNumber: '20' },
+	{ name: 'Marcus Smart', position: 'Guard', jerseyNumber: '36' },
+	{ name: 'Enes Kanter', position: 'Forward', jerseyNumber: '11' },
+	{ name: 'Daniel Theis', position: 'Center', jerseyNumber: '27' }
+];
