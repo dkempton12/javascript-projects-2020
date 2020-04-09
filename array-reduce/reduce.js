47d2ecfa58edf4d070dfc23f4d631c2e4f966b5f
Dan Kempton <kemptondan@gmail.com>
2020-04-09 17:06:07 -0400

    Adding reduce function

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -1,1 +1,4 @@
 const numbers = [ 12, 93, 120, 88, 24, 8, 36, 33, 34, 12, 6, 5, 9, 50, 45 ];
+
+const addUpNumbers = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(addUpNumbers);
