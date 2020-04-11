c4ea78e8757290d010743ceb680a1e0082521f39
Dan Kempton <kemptondan@gmail.com>
2020-04-11 12:25:25 -0400

    Adding map method

--- capitalize-words/capitalize.js
+++ capitalize-words/capitalize.js
@@ -1,3 +1,4 @@
 const sentence = `this is a sentence`;
 
-const capitalizedWords = sentence.split(' ');
+const capitalizedWords = sentence.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
+console.log(capitalizedWords);
