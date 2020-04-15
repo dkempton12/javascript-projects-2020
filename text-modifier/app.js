4cef0bec290830556a22a921b9eba017236828ae
Dan Kempton <kemptondan@gmail.com>
2020-04-15 13:43:33 -0400

    Adding a find method to transformText function that finds the radio input that is selected

--- text-modifier/app.js
+++ text-modifier/app.js
@@ -3,7 +3,10 @@ const textArea = document.querySelector('.text');
 const result = document.querySelector('.result');
 const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
 
-function transformText(text) {}
+function transformText(text) {
+	const filter = filterInputs.find((input) => input.checked).value;
+	console.log(filter);
+}
 
 // Listening for text value events in the textarea input
 textArea.addEventListener('input', (event) => transformText(event.target.value));
