92a78bcfabf67692aeec28e9086f838101555f74
Dan Kempton <kemptondan@gmail.com>
2020-04-15 13:34:10 -0400

    Grabbing the elements in JS

---
+++ text-modifier/app.js
@@ -0,0 +1,4 @@
+// Grabbing elements
+const textArea = document.querySelector('.text');
+const result = document.querySelector('.result');
+const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
