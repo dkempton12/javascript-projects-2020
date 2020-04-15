087cf6f10307d7311f3f07e73cf77c53c3c4eda5
Dan Kempton <kemptondan@gmail.com>
2020-04-15 17:39:06 -0400

    Adding a modifyText function that maps the letters typed into the textarea

--- text-modifier/app.js
+++ text-modifier/app.js
@@ -16,6 +16,9 @@ const filters = {
 function transformText(text) {
 	const filter = filterInputs.find((input) => input.checked).value;
 	console.log(filter);
+
+	// Looping over each letter that is typed into the textarea
+	const modifyText = Array.from(text).map(filters[filter]); // Filter lookup that looks up to the filter methods
 }
 
 // Listening for text value events in the textarea input
