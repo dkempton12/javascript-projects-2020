033ba98cb30fe1cd0a40fccbd9b425caa3cc91c3
Dan Kempton <kemptondan@gmail.com>
2020-04-15 17:36:01 -0400

    Adding a filters object that contains methods for filtering the text based on input selected

--- text-modifier/app.js
+++ text-modifier/app.js
@@ -3,6 +3,16 @@ const textArea = document.querySelector('.text');
 const result = document.querySelector('.result');
 const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
 
+// Object that contains the filter methods for transforming text
+const filters = {
+	sarcastic() {},
+
+	funky() {},
+
+	unstructured() {}
+};
+
+// Function that filters the text
 function transformText(text) {
 	const filter = filterInputs.find((input) => input.checked).value;
 	console.log(filter);
