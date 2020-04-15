6098ba72fb99047231000030e0168a8a91969d44
Dan Kempton <kemptondan@gmail.com>
2020-04-15 13:37:17 -0400

    Adding event listener on the textarea element and adding the transformText function

--- text-modifier/app.js
+++ text-modifier/app.js
@@ -2,3 +2,8 @@
 const textArea = document.querySelector('.text');
 const result = document.querySelector('.result');
 const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
+
+function transformText(text) {}
+
+// Listening for text value events in the textarea input
+textArea.addEventListener('input', (event) => transformText(event.target.value));
