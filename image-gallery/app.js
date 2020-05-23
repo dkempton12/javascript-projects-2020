da100ba0235ce7ae370e08043d72557ff4528e66
Dan Kempton <kemptondan@gmail.com>
2020-05-23 13:32:45 -0400

    Checking if a gallery element is present

--- image-gallery/app.js
+++ image-gallery/app.js
@@ -1,3 +1,7 @@
-function Gallery(gallery) {}
+function Gallery(gallery) {
+	if (!gallery) {
+		throw new Error(`No gallery present`);
+	}
+}
 
 const gallery = Gallery(document.querySelector('.gallery'));
