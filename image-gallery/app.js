7ddb84ec6a8e88707089dcf0db6c044e5cf3373f
Dan Kempton <kemptondan@gmail.com>
2020-05-23 13:37:16 -0400

    Selecting gallery elements

--- image-gallery/app.js
+++ image-gallery/app.js
@@ -2,6 +2,14 @@ function Gallery(gallery) {
 	if (!gallery) {
 		throw new Error(`No gallery present`);
 	}
+
+	// select elements
+	const images = Array.from(gallery.querySelectorAll('img'));
+	const modal = document.querySelector('.modal');
+	const previousButton = modal.querySelector('.previous');
+	const nextButton = modal.querySelector('.next');
+	// declare current image you're targeting in the gallery
+	let currentImage;
 }
 
 const gallery = Gallery(document.querySelector('.gallery'));
