1115127703ffb05a86530dc8f2e2026fccc9e6a1
Dan Kempton <kemptondan@gmail.com>
2020-05-29 13:17:23 -0400

    Moving functions outside of the Gallery prototype, in order to attach them to the prototype

--- image-gallery-prototype-version/app/index.js
+++ image-gallery-prototype-version/app/index.js
@@ -9,65 +9,6 @@ function Gallery(gallery) {
 	this.previousButton = this.modal.querySelector('.previous');
 	this.nextButton = this.modal.querySelector('.next');
 
-	function openModal() {
-		if (this.modal.matches('.open')) {
-			console.info('Modal is already open!');
-		}
-		this.modal.classList.add('open');
-		window.addEventListener('keyup', handleKeyUps);
-		this.nextButton.addEventListener('click', showNextImage);
-		this.previousButton.addEventListener('click', showPreviousImage);
-	}
-
-	function closeModal() {
-		this.modal.classList.remove('open');
-		window.removeEventListener('keyup', handleKeyUps);
-		this.nextButton.removeEventListener('click', showNextImage);
-		this.previousButton.removeEventListener('click', showPreviousImage);
-	}
-
-	function closeModalWhenClickIsOutsideInnerModal(event) {
-		if (event.target === event.currentTarget) {
-			closeModal();
-		}
-	}
-
-	function handleKeyUps(event) {
-		if (event.key === 'Escape') {
-			return closeModal();
-		}
-		if (event.key === 'ArrowRight') {
-			return showNextImage();
-		}
-
-		if (event.key === 'ArrowLeft') {
-			return showPreviousImage();
-		}
-	}
-
-	function showNextImage() {
-		showImage(currentImage.nextElementSibling || gallery.firstElementChild);
-	}
-
-	function showPreviousImage() {
-		showImage(currentImage.previousElementSibling || gallery.lastElementChild);
-	}
-
-	function showImage(element) {
-		// show image data in the modal
-		// img
-		this.modal.querySelector('img').src = element.src;
-		// img title
-		this.modal.querySelector('h2').textContent = element.title;
-		// img caption
-		this.modal.querySelector('p').textContent = element.dataset.description;
-
-		// set currentImage variable equal to img element being passed in
-		currentImage = element;
-		// run openModal
-		openModal();
-	}
-
 	this.images.forEach((image) =>
 		image.addEventListener('click', function(event) {
 			showImage(event.target);
@@ -83,6 +24,65 @@ function Gallery(gallery) {
 	);
 
 	this.modal.addEventListener('click', closeModalWhenClickIsOutsideInnerModal);
+}
+
+function openModal() {
+	if (this.modal.matches('.open')) {
+		console.info('Modal is already open!');
+	}
+	this.modal.classList.add('open');
+	window.addEventListener('keyup', handleKeyUps);
+	this.nextButton.addEventListener('click', showNextImage);
+	this.previousButton.addEventListener('click', showPreviousImage);
+}
+
+function closeModal() {
+	this.modal.classList.remove('open');
+	window.removeEventListener('keyup', handleKeyUps);
+	this.nextButton.removeEventListener('click', showNextImage);
+	this.previousButton.removeEventListener('click', showPreviousImage);
+}
+
+function closeModalWhenClickIsOutsideInnerModal(event) {
+	if (event.target === event.currentTarget) {
+		closeModal();
+	}
+}
+
+function handleKeyUps(event) {
+	if (event.key === 'Escape') {
+		return closeModal();
+	}
+	if (event.key === 'ArrowRight') {
+		return showNextImage();
+	}
+
+	if (event.key === 'ArrowLeft') {
+		return showPreviousImage();
+	}
+}
+
+function showNextImage() {
+	showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+}
+
+function showPreviousImage() {
+	showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+}
+
+function showImage(element) {
+	// show image data in the modal
+	// img
+	this.modal.querySelector('img').src = element.src;
+	// img title
+	this.modal.querySelector('h2').textContent = element.title;
+	// img caption
+	this.modal.querySelector('p').textContent = element.dataset.description;
+
+	// set currentImage variable equal to img element being passed in
+	currentImage = element;
+	// run openModal
+	openModal();
 }
 
 const gallery = new Gallery(document.querySelector('.gallery'));
