b6faafe9b64f42d4a85c4be29f63e06955ed8155
Dan Kempton <kemptondan@gmail.com>
2020-06-04 13:33:48 -0400

    Adding a setTimeout function that resolves the coffee Promises

--- promises-javascript/index.js
+++ promises-javascript/index.js
@@ -2,5 +2,8 @@
 function makeCoffees(addOns = []) {
 	const coffeePromise = new Promise(function(resolve, reject) {
 		const timeToMakeCoffee = 1000 + addOns.length * 300;
+		setTimeout(function() {
+			resolve(`Your coffee is ready with: ${addOns.join(', ')}`);
+		}, timeToMakeCoffee);
 	});
 }
