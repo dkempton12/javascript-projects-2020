6918d4e258e41124778d899d109f1f8521516fc7
Dan Kempton <kemptondan@gmail.com>
2020-06-04 13:31:49 -0400

    Adding a custom time variable - timeToMakeCoffee

--- promises-javascript/index.js
+++ promises-javascript/index.js
@@ -1,4 +1,6 @@
 // Promise function
 function makeCoffees(addOns = []) {
-	const coffeePromise = new Promise(function(resolve, reject) {});
+	const coffeePromise = new Promise(function(resolve, reject) {
+		const timeToMakeCoffee = 1000 + addOns.length * 300;
+	});
 }
