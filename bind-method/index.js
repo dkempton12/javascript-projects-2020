78afc928585110aedd1be28d28b1a75fcdd68938
Dan Kempton <kemptondan@gmail.com>
2020-06-11 16:19:45 -0400

    Adding unbounded example

--- bind-method/index.js
+++ bind-method/index.js
@@ -4,3 +4,8 @@ const module = {
 		return this.randomNumber;
 	}
 };
+
+// example where the this vale of getRandomNumber is equal to the Window
+// In other words, this variable being declared is being stored on the Window (therefor, this = Window)
+const getRandomNumberUnbound = module.getRandomNumber;
+console.log(getRandomNumberUnbound());
