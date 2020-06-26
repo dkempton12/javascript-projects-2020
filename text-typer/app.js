87caffcddc864e255af837ae6ce3c95e32c80dbc
Dan Kempton <kemptondan@gmail.com>
2020-06-26 12:25:40 -0400

    Adding type function for typing the letters

--- text-typer/app.js
+++ text-typer/app.js
@@ -8,9 +8,17 @@ function generateRandomNumber(min, max, multiplier = Math.random()) {
 	return multiplier * (max - min) + min;
 }
 
-function type(element) {
+async function type(element) {
 	const text = element.textContent;
 	const textSoFar = '';
+	for (const letter of text) {
+		// set random time for the typing
+		const { typeMin, typeMax } = element.dataset;
+		const timeToWait = generateRandomNumber(typeMin, typeMax);
+		await timer(timeToWait);
+		textSoFar += letter;
+		textSoFar = element.textContent;
+	}
 }
 
 type();
