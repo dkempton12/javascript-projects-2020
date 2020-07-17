e8bfa9c8c55644eed1ead39823362c04ffc3c52f
Dan Kempton <kemptondan@gmail.com>
2020-07-17 16:56:46 -0400

    Refactoring - all code now stored in acceptTerms, and runs on page load

--- intersection-observer-api/scroll.js
+++ intersection-observer-api/scroll.js
@@ -1,24 +1,3 @@
-const text = document.querySelector('.text');
-
-const button = document.querySelector('.accept');
-
-// Displays button if intersectionRatio is equal to 1
-function observationCallBack(payback) {
-	if (payback[0].intersectionRatio === 1) {
-		button.disabled = false;
-		// ob.unobserve(text.lastElementChild);
-	} else {
-		button.disabled = true;
-	}
-}
-// IntersectionObserver API - takes a root (parent) and threshold ratio
-const ob = new IntersectionObserver(observationCallBack, {
-	root: text,
-	threshold: 1 // Threshold of 1 refers to when the element is 100% visible on the page
-});
-
-ob.observe(text.lastElementChild); // Watching for last element in the text div
-
 function acceptTerms() {
 	const text = document.querySelector('.text');
 	if (!text) {
@@ -39,4 +18,8 @@ function acceptTerms() {
 		root: text,
 		threshold: 1
 	});
+
+	intersection.observe(text.lastElementChild);
 }
+
+acceptTerms();
